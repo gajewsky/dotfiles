0c2ed12470e7d32f82f71004e17c13f0973c674d
Marcin Gajewski <code@gajewsky.it>
2019-11-09 23:21:19 +0100

    Base fish setup

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+stty intr ^e
+
+alias ls="exa"
+alias cat="bat"
+alias vim="nvim"
+alias reload!="source ~/.config/fish/config.fish"
+
