b197a3dee28d866e2f25e66f88024b26f262716c
Marcin Gajewski <code@gajewsky.it>
2019-11-13 20:55:44 +0100

    Add aliases and tpm

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,10 @@
+. ~/.config/fish/aliases.fish
+
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
-
-alias ls="exa -a"
-alias cat="bat"
-alias vim="nvim"
-alias reload!="source ~/.config/fish/config.fish"
 
 export VIMRC=$HOME/.vimrc
 
