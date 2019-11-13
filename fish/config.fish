ed738e24d8f25390fef9456016c14f2fc29c805f
Marcin Gajewski <code@gajewsky.it>
2019-11-13 23:53:15 +0100

    Configure yay

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 . ~/.config/fish/aliases.fish
 
+set -U FZF_LEGACY_KEYBINDINGS 0
+
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
