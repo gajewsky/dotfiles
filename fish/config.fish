3762c5ee96e2d596f3c9567cb6e5bf6d91c53548
Marcin Gajewski <code@gajewsky.it>
2019-11-17 22:59:00 +0100

    Clean up dotbot

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,12 @@
 . ~/.config/fish/aliases.fish
 
-set -U FZF_LEGACY_KEYBINDINGS 0
-
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
 
-export VIMRC=$HOME/.vimrc
+set -U FZF_LEGACY_KEYBINDINGS 0
+set -x DOTFILES ~/.dotfiles
+set -x VIMRC $HOME/.vimrc
 
