a42f1662d1f8b20a7ff5fee28621fb4712cc6c3b
Marcin Gajewski <gajewski@pm.me>
2023-06-02 11:55:36 +0200

    Update fisher plugins once per day

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,5 @@
 source ~/.config/fish/aliases.fish
 
-set -U FZF_COMPLETE 0
-set -U FZF_LEGACY_KEYBINDINGS 0
-set -U FZF_DEFAULT_COMMAND 'rg --files --follow --no-ignore-vcs --hidden --glob "!{node_modules/*,.git/*,sorbet/*}"'
-
-set -U FZF_DEFAULT_OPTS '--layout=reverse --height 40%'
-set -U FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-
 set -x DOTFILES ~/.dotfiles
 set -x VIMRC $HOME/.vimrc
 set -x EDITOR /usr/bin/nvim
@@ -19,4 +12,10 @@ set PATH ~/git-fuzzy/bin $PATH
 
 if test -f /opt/dev/dev.fish
   source /opt/dev/dev.fish
+end
+
+set plugins ~/.config/fish/plugins/
+
+for file in $plugins/*.fish
+    source $file
 end
---
+++ fish/plugins/pure.fish
@@ -0,0 +1,3 @@
+set pure_color_primary green
+set -g async_prompt_functions _pure_prompt_git
+
