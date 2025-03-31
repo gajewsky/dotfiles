c7e9d77c4d5072ffc40c9b112790ba3ade1142d6
Marcin Gajewski <gajewski@pm.me>
2025-03-31 13:43:47 +0200

    Fix paths

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,29 @@
 source ~/.config/fish/aliases.fish
 
+# macOS-specific paths
+switch (uname)
+  case Darwin
+    if test -d /opt/homebrew/bin
+      fish_add_path /opt/homebrew/bin
+      fish_add_path /opt/homebrew/sbin
+    end
+
+    if test -x /opt/homebrew/bin/pyenv
+      set -gx PYENV_ROOT $HOME/.pyenv
+      fish_add_path $PYENV_ROOT/bin
+      pyenv init - | source
+    end
+end
+
 set fish_greeting
 set -x DOTFILES ~/.dotfiles
 set -x VIMRC $HOME/.vimrc
 set -x EDITOR /usr/bin/nvim
 set -x MYVIMRC ~/.dotfiles/vim/.vimrc
-# xset r rate 250 100
-set PATH ~/bin $PATH
-set PATH ~/.local/bin $PATH
-set PATH ~/.cargo/bin $PATH
-set PATH ~/git-fuzzy/bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+set -gx PATH ~/.cargo/bin $PATH
+set -gx PATH ~/git-fuzzy/bin $PATH
 
 if test -f /opt/dev/dev.fish
     source /opt/dev/dev.fish
@@ -18,7 +32,7 @@ end
 if status is-interactive
   export ZELLIJ_CONFIG_DIR=$HOME/.config/zellij
 
-  if [ "$TERM" = "xterm-ghostty" ]
+  if [ "$TERM" = "xterm-ghostty" ] && command -v zellij >/dev/null
     eval (zellij setup --generate-auto-start fish | string collect)
   end
 end
@@ -31,8 +45,4 @@ set plugins ~/.config/fish/plugins/
 
 for file in $plugins/*.fish
     source $file
-end
-
-if test -d ~/.asdf
-    source ~/.asdf/asdf.fish
 end
