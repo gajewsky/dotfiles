514b9a05c21dc6171bb2d5454cb3c108f25311ec
Marcin Gajewski <gajewski@pm.me>
2025-04-04 13:52:54 +0200

    Remove ugly fish plugins

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,16 @@
 source ~/.config/fish/aliases.fish
 
-# macOS-specific paths
-switch (uname)
-  case Darwin
-    if test -d /opt/homebrew/bin
-      fish_add_path /opt/homebrew/bin
-      fish_add_path /opt/homebrew/sbin
-    end
-
-    if test -x /opt/homebrew/bin/pyenv
-      set -gx PYENV_ROOT $HOME/.pyenv
-      fish_add_path $PYENV_ROOT/bin
-      pyenv init - | source
-    end
-end
-
 set fish_greeting
 set -x DOTFILES ~/.dotfiles
 set -x VIMRC $HOME/.vimrc
 set -x EDITOR /usr/bin/nvim
 set -x MYVIMRC ~/.dotfiles/vim/.vimrc
+
+# Add homebrew to PATH for Apple Silicon Macs
+if test -d /opt/homebrew/bin
+    set -gx PATH /opt/homebrew/bin $PATH
+end
+
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
 set -gx PATH ~/.cargo/bin $PATH
@@ -29,12 +20,11 @@ if test -f /opt/dev/dev.fish
     source /opt/dev/dev.fish
 end
 
-if status is-interactive
-  export ZELLIJ_CONFIG_DIR=$HOME/.config/zellij
+set -gx ZELLIJ_CONFIG_DIR $HOME/.config/zellij
 
-  if [ "$TERM" = "xterm-ghostty" ] && command -v zellij >/dev/null
-    eval (zellij setup --generate-auto-start fish | string collect)
-  end
+# Fix for prompt breaking in zellij
+if set -q ZELLIJ
+    set -gx pure_enable_single_line_prompt true
 end
 
 if command -v zoxide >/dev/null
@@ -45,4 +35,11 @@ set plugins ~/.config/fish/plugins/
 
 for file in $plugins/*.fish
     source $file
+end
+
+# Auto-launch zellij when opening ghostty
+if status is-interactive
+    and not set -q ZELLIJ
+    and command -v zellij >/dev/null
+    eval (zellij setup --generate-auto-start fish | string collect)
 end
--- fish/plugins/pure.fish
+++
@@ -1,3 +0,0 @@
-set pure_color_primary green
-set -g async_prompt_functions _pure_prompt_git
-
