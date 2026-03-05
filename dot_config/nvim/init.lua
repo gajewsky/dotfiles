accdef5b303238736fa10ef7bf94a2085a5efa79
Marcin Gajewski <marcin.gajewski@shopify.com>
2026-03-05 17:57:49 +0100

    Switch nvim config to LazyVim
    Replace custom nvim setup with LazyVim starter. Keep comma as leader,
    ,f for file search, ,a for live grep. Use gruvbox colorscheme and
    remove dashboard logo.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,15 +1,2 @@
------------------------------------------------------------
--- Import Lua modules
------------------------------------------------------------
-require('plugins/lazy')             -- plugin manager
-require('envs')                     -- env variables
-require('settings')                 -- settings
-require('keymaps')                  -- keymaps
-require('plugins/nvim-tree')        -- file manager
-require('plugins/lualine')          -- statusline
-require('plugins/nvim-cmp')         -- autocomplete
-require('plugins/nvim-lspconfig')   -- LSP settings
-require('plugins/nvim-treesitter')  -- tree-sitter interface
-require('plugins/gitsigns')         -- git in gutter
-require('plugins/telescope')        -- fuzzy finder
-require('plugins/others')           -- minor plugins settings
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- dot_config/nvim/lua/envs.lua
+++
@@ -1,6 +0,0 @@
------------------------------------------------------------
--- Env configuration file: envs of neovim
--- and plugins.
------------------------------------------------------------
-
-vim.fn.setenv("FZF_DEFAULT_COMMAND", 'rg --files --follow --no-ignore-vcs --hidden --glob "!{node_modules/*,.git/*,sorbet/*}"')
